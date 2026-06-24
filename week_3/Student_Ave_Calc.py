0868c931d40be8e43cf2132e91bf718c2433151d
nikitaodul0-create <nikitaodul0@gmail.com>
2026-06-24 14:24:17 +0800

    Create Student_Ave_Calc.py

---
+++ week_3/Student_Ave_Calc.py
@@ -0,0 +1,20 @@
+choice = "y"
+
+while choice == "y":
+
+    quiz_1 = float(input("Enter Quiz 1 mark: "))
+    quiz_2 = float(input("Enter Quiz 2 mark: "))
+    quiz_3 = float(input("Enter Quiz 3 mark: "))
+
+    average = (quiz_1 + quiz_2 + quiz_3) / 3
+
+    print("Average =", average)
+
+    if average >= 50:
+        print("Pass")
+    else:
+        print("Fail")
+
+    choice = input("Continue? Select Y/N: ").lower()
+
+print("Program Ended")
