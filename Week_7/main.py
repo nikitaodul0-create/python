696f2ed89b7ed18af77704125692c8884ede9fbc
nikitaodul0-create <nikitaodul0@gmail.com>
2026-07-08 12:54:11 +0800

    Update main.py

--- Week_7/main.py
+++ Week_7/main.py
@@ -1,1 +1,15 @@
+from food_order import calculate_total
+
+
+def main():
+    price = float(input("Price (RM): "))
+    quantity = int(input("Quantity: "))
+
+    total = calculate_total(price, quantity)
+
+    print(f"Total Payment = RM {total}")
+
+
+if __name__ == "__main__":
+    main()
 
