5687b7fb03ccf8c954b8e41ae062e381ed7515bf
nikitaodul0-create <nikitaodul0@gmail.com>
2026-07-08 12:42:27 +0800

    Update main.py

--- Week_5/main.py
+++ Week_5/main.py
@@ -1,1 +1,22 @@
+from utils import calculate_total, print_receipt
 
+
+# Get customer information
+
+customer = input("Customer name: ")
+
+coffee = int(input("Coffee quantity: "))
+
+tea = int(input("Tea quantity: "))
+
+sandwich = int(input("Sandwich quantity: "))
+
+
+# Calculate bill
+
+total = calculate_total(coffee, tea, sandwich)
+
+
+# Display receipt
+
+print_receipt(customer, coffee, tea, sandwich, total)
