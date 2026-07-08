d63b9c50c0ac0d38910ca8b09e50086c09556a87
nikitaodul0-create <nikitaodul0@gmail.com>
2026-07-08 12:54:49 +0800

    Update test_food_order.py

--- Week_7/test_food_order.py
+++ Week_7/test_food_order.py
@@ -1,1 +1,26 @@
+from food_order import calculate_total
+
+
+def test_order1():
+    assert calculate_total(10, 2) == 20
+
+
+def test_order2():
+    assert calculate_total(10, 3) == 30
+
+
+def test_order3():
+    assert calculate_total(10, 4) == 40
+
+
+def test_order4():
+    assert calculate_total(10, 1) == 10
+
+
+def test_invalid_price():
+    assert calculate_total(-10, 2) == "invalid price"
+
+
+def test_invalid_quantity():
+    assert calculate_total(10, -2) == "invalid quantity"
 
