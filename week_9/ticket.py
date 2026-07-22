a76db43af8dbcf54ed40acf3b62f7d00a468d2dc
nikitaodul0-create <nikitaodul0@gmail.com>
2026-07-22 15:17:33 +0800

    Update ticket.py

--- week_9/ticket.py
+++ week_9/ticket.py
@@ -1,1 +1,24 @@
+def create_ticket():
 
+    print("=== IT Helpdesk Ticket ===")
+
+    name = input("Enter student name: ")
+    student_id = input("Enter student ID: ")
+    issue = input("Enter issue: ")
+    location = input("Enter location: ")
+
+    print("\nPriority Level")
+    print("1. High")
+    print("2. Medium")
+    print("3. Low")
+
+    choice = input("Choose priority (1-3): ")
+
+    if choice == "1":
+        priority = "High"
+    elif choice == "2":
+        priority = "Medium"
+    else:
+        priority = "Low"
+
+    return name, student_id, issue, location, priority
