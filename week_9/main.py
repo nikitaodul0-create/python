b5dd3eaf43ab6356c7d909358396cae772d010b8
nikitaodul0-create <nikitaodul0@gmail.com>
2026-07-22 15:18:22 +0800

    Update main.py

--- week_9/main.py
+++ week_9/main.py
@@ -1,1 +1,21 @@
+from ticket import create_ticket
+from display import display_ticket
 
+
+def main():
+
+    # Create a new ticket
+    name, student_id, issue, location, priority = create_ticket()
+
+    # Display the completed ticket
+    display_ticket(
+        name,
+        student_id,
+        issue,
+        location,
+        priority
+    )
+
+
+if __name__ == "__main__":
+    main()
