73abb10d5825449ea3f6a91baf4f981bb89486b1
nikitaodul0-create <nikitaodul0@gmail.com>
2026-07-22 15:17:59 +0800

    Update display.py

--- week_9/display.py
+++ week_9/display.py
@@ -1,1 +1,18 @@
+def display_ticket(name, student_id, issue, location, priority):
 
+    if priority == "High":
+        technician = "Ahmad"
+    elif priority == "Medium":
+        technician = "Siti"
+    else:
+        technician = "Ali"
+
+    print("\n========== HELPDESK TICKET ==========")
+    print(f"Student Name : {name}")
+    print(f"Student ID   : {student_id}")
+    print(f"Issue        : {issue}")
+    print(f"Location     : {location}")
+    print(f"Priority     : {priority}")
+    print(f"Technician   : {technician}")
+    print("Status       : Pending")
+    print("====================================")
